fb0f5bda50063ceef4061a71106a98aa49edd9fb
aldao90020 <90020@sistemas.frc.utn.edu.ar>
2024-09-10 13:55:00 -0300

    clase 06/09

--- APIRest01/APIRest01/obj/Debug/net8.0/APIRest01.AssemblyInfo.cs
+++ APIRest01/APIRest01/obj/Debug/net8.0/APIRest01.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Practica01")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("APIRest01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b924989bae07fd07940a299232cfa35cdc27489d")]
-[assembly: System.Reflection.AssemblyProductAttribute("Practica01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Practica01")]
+[assembly: System.Reflection.AssemblyProductAttribute("APIRest01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("APIRest01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- APIRest01/APIRest01/obj/Debug/net8.0/APIRest01.MvcApplicationPartsAssemblyInfo.cs
+++ APIRest01/APIRest01/obj/Debug/net8.0/APIRest01.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Practica01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b924989bae07fd07940a299232cfa35cdc27489d")]
-[assembly: System.Reflection.AssemblyProductAttribute("Practica01")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Practica01")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
